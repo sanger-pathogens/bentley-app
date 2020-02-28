1c1197215b1dca0cf84eb6a884ed7f9bce0e4bd4
Gareth Peat <gp16@sanger.ac.uk>
2020-02-28 14:18:36 +0000

    Add point groups to map

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -5,14 +5,21 @@ import raw from 'raw.macro';
 import Page from './Page';
 import WorldMap from './WorldMap';
 import Content from './Content';
+import locationsJuno from '../content/locationsJuno';
+import locationsGPS from '../content/locationsGPS';
+
+const pointGroups = [
+  { name: 'Juno', fill: '#F9A03F', points: locationsJuno },
+  { name: 'GPS', fill: '#9AD4D6', points: locationsGPS },
+];
 
 const mdHome = raw('../content/home.md');
 
 const PageHome = () => {
   return (
     <React.Fragment>
-      <Box bgcolor="black">
-        <WorldMap />
+      <Box p={2} bgcolor="black">
+        <WorldMap pointGroups={pointGroups} />
       </Box>
       <Page>
         <Box color="white">
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -4,7 +4,7 @@ import { geoNaturalEarth1, geoPath, geoGraticule10 } from 'd3';
 import { feature } from 'topojson';
 import world from 'world-atlas/countries-110m.json';
 
-const WorldMap = () => {
+const WorldMap = ({ pointGroups }) => {
   // update if container dimensions change
   const nodeRef = useRef();
   const size = useMeasure(nodeRef);
@@ -29,14 +29,54 @@ const WorldMap = () => {
         height={height}
         style={{ position: 'absolute' }}
         fill="none"
-        stroke="white"
+        stroke="#444"
         viewBox={`0 0 ${width ? width : 0} ${height ? height : 0}`}
       >
-        <path d={path(graticule)} strokeWidth="0.5" />
-        <path d={path(outline)} />
-        {features.map((feature, i) => (
-          <path key={i} d={path(feature)} strokeWidth="1.5" fill="none" />
-        ))}
+        <g>
+          <path d={path(graticule)} strokeWidth="0.5" />
+        </g>
+        <g>
+          <path d={path(outline)} />
+          {features.map((feature, i) => (
+            <path fill="black" key={i} d={path(feature)} strokeWidth="1" />
+          ))}
+        </g>
+        {pointGroups ? (
+          <g>
+            {pointGroups.map((group, i) => (
+              <g key={i} fill={group.fill}>
+                {group.points.map((point, j) => {
+                  const [cx, cy] = projection([
+                    point.longitude,
+                    point.latitude,
+                  ]);
+                  return <circle key={j} {...{ cx, cy, r: 4 }} />;
+                })}
+              </g>
+            ))}
+          </g>
+        ) : null}
+        {pointGroups ? (
+          <g>
+            {pointGroups.map((group, i) => (
+              <React.Fragment key={i}>
+                <circle
+                  fill={group.fill}
+                  {...{ cx: 20, cy: 600 - 20 * (i + 1), r: 4 }}
+                />
+                <text
+                  stroke="none"
+                  fill="#999"
+                  alignmentBaseline="middle"
+                  x={30}
+                  y={600 - 20 * (i + 1)}
+                >
+                  {group.name}
+                </text>
+              </React.Fragment>
+            ))}
+          </g>
+        ) : null}
       </svg>
     </div>
   );
