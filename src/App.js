cb84b4777a3f9d85a4c38e450449b086c278ab30
Gareth Peat <gp16@sanger.ac.uk>
2020-03-14 20:57:08 +0000

    Use ThemeProvider with responsiveFontSizes and overrides

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
+import {
+  ThemeProvider,
+  createMuiTheme,
+  responsiveFontSizes,
+} from '@material-ui/core/styles';
 
 import PageHome from './components/PageHome';
+
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      type: 'dark',
+      // primary: { main: '#7293BC' }, // Christine's blue
+      primary: { main: '#00bcd4' },
+      secondary: { main: '#651fff' },
+    },
+    status: {
+      danger: 'orange',
+    },
+    props: {
+      MuiButtonBase: {
+        disableRipple: true,
+      },
+      MuiPaper: {
+        elevation: 0,
+        square: true,
+      },
+      MuiLink: {
+        underline: 'none',
+      },
+    },
+  })
+);
 
 const App = () => (
   <React.Fragment>
     <CssBaseline />
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <PageHome />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <PageHome />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   </React.Fragment>
 );
 
