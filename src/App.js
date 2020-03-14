095ddd1f1aff4861acce5509af26b66a352e9927
Gareth Peat <gp16@sanger.ac.uk>
2020-03-14 19:54:37 +0000

    Use material ui's CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 import PageHome from './components/PageHome';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <PageHome />
-      </Route>
-    </Switch>
-  </Router>
+  <React.Fragment>
+    <CssBaseline />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <PageHome />
+        </Route>
+      </Switch>
+    </Router>
+  </React.Fragment>
 );
 
 export default App;
