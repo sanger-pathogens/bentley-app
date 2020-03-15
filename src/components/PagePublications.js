1ac548997372de99562c9a2513e6b057be695c23
Gareth Peat <gp16@sanger.ac.uk>
2020-03-15 01:21:50 +0000

    Add more publications link

--- src/components/PagePublications.js
+++ src/components/PagePublications.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Link } from '@material-ui/core';
 
 import Page from './Page';
 import Publications from './Publications';
@@ -10,6 +10,15 @@ const PagePublications = () => {
       <Box>
         <Typography variant="h2">Publications</Typography>
         <Publications />
+      </Box>
+      <Box pt={3}>
+        <Link
+          href={
+            'https://scholar.google.co.uk/citations?user=rywPBOQAAAAJ&hl=en'
+          }
+        >
+          More publications
+        </Link>
       </Box>
     </Page>
   );
