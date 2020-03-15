f56dd2676dc32590a625a9d2935179a5870cd8c2
Gareth Peat <gp16@sanger.ac.uk>
2020-03-15 16:58:22 +0000

    Add three kingdoms pie to TreeOfLife

--- src/components/TreeOfLife.js
+++ src/components/TreeOfLife.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import useMeasure from 'use-measure';
 import raw from 'raw.macro';
-import { hierarchy, cluster, ascending, max } from 'd3';
+import { hierarchy, cluster, ascending, max, extent, arc } from 'd3';
 
 import newickParser from '../newickParser';
 
@@ -64,6 +64,10 @@ function maxLength(d) {
   return d.data.length + (d.children ? max(d.children, maxLength) : 0);
 }
 
+function deg2Rad(deg) {
+  return (deg * Math.PI) / 180;
+}
+
 const TreeOfLife = () => {
   // update if container dimensions change
   const nodeRef = useRef();
@@ -71,7 +75,7 @@ const TreeOfLife = () => {
   const { width, height } = size;
 
   const outerRadius = width / 2;
-  const innerRadius = outerRadius - 170;
+  const innerRadius = outerRadius - 200;
 
   const root = hierarchy(treeOfLifeData, d => d.branchset)
     .sum(d => (d.branchset ? 0 : 1))
@@ -85,6 +89,42 @@ const TreeOfLife = () => {
 
   clusterer(root);
   setRadius(root, (root.data.length = 0), innerRadius / maxLength(root));
+
+  const allNodes = root.descendants();
+  const nodeHalfAngle = 360 / allNodes.length;
+  const bacteriaNode = allNodes.find(d => d.data.name === 'Bacteria');
+  const archaeaNode = allNodes.find(d => d.data.name === 'Archaea');
+  const eukaryotaNode = allNodes.find(d => d.data.name === 'Eukaryota');
+  const bacteriaAngleRange = extent(bacteriaNode.descendants(), d => d.x);
+  const archaeaAngleRange = extent(archaeaNode.descendants(), d => d.x);
+  const eukaryotaAngleRange = extent(eukaryotaNode.descendants(), d => d.x);
+  const arcs = [
+    {
+      data: { name: 'bacteria', color: '#006064' },
+      startAngle: deg2Rad(bacteriaAngleRange[0] - nodeHalfAngle),
+      endAngle: deg2Rad(bacteriaAngleRange[1] + nodeHalfAngle),
+      padAngle: 0,
+      index: 0,
+    },
+    {
+      data: { name: 'archaea', color: '#e65100' },
+      startAngle: deg2Rad(archaeaAngleRange[0] - nodeHalfAngle),
+      endAngle: deg2Rad(archaeaAngleRange[1] + nodeHalfAngle),
+      padAngle: 0,
+      index: 1,
+    },
+    {
+      data: { name: 'eukaryota', color: '#1a237e' },
+      startAngle: deg2Rad(eukaryotaAngleRange[0] - nodeHalfAngle),
+      endAngle: deg2Rad(eukaryotaAngleRange[1] + nodeHalfAngle),
+      padAngle: 0,
+      index: 2,
+    },
+  ];
+
+  const arcGenerator = arc()
+    .innerRadius(innerRadius + 10)
+    .outerRadius(innerRadius + 30);
 
   const links = root.links();
   const leaves = root.leaves();
@@ -100,8 +140,18 @@ const TreeOfLife = () => {
         }`}
       >
         <g stroke="white" fill="none">
-          {links.map(link => (
-            <path d={linkVariable(link)} />
+          {arcs.map(arc => (
+            <path
+              key={arc.index}
+              stroke="none"
+              fill={arc.data.color}
+              d={arcGenerator(arc)}
+            />
+          ))}
+        </g>
+        <g stroke="white" fill="none">
+          {links.map((link, i) => (
+            <path key={i} d={linkVariable(link)} />
           ))}
         </g>
         <g fontFamily="sans-serif">
@@ -109,11 +159,12 @@ const TreeOfLife = () => {
             const isOurSpecies = ourSpecies.indexOf(leaf.data.name) >= 0;
             return (
               <text
+                key={leaf.data.name}
                 fontSize={isOurSpecies ? '14px' : '10px'}
                 fill={isOurSpecies ? 'white' : '#777'}
                 dy=".31em"
                 transform={`rotate(${leaf.x - 90}) translate(${innerRadius +
-                  4},0)${leaf.x < 180 ? '' : ' rotate(180)'}`}
+                  34},0)${leaf.x < 180 ? '' : ' rotate(180)'}`}
                 textAnchor={leaf.x < 180 ? 'start' : 'end'}
               >
                 {leaf.data.name.replace(/_/g, ' ')}
