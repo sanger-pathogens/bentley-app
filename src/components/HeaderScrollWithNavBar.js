2b1844c862d3a902e7ca128b79ca8e394bd09423
Gareth Peat <gp16@sanger.ac.uk>
2020-03-15 14:55:54 +0000

    Change NavBar colour on scroll

--- src/components/HeaderScrollWithNavBar.js
+++ src/components/HeaderScrollWithNavBar.js
@@ -1,31 +1,56 @@
 import React from 'react';
-import { Box, Grid, Button, Typography, Link } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  Button,
+  Typography,
+  Link,
+  useScrollTrigger,
+} from '@material-ui/core';
+
+const ElevationScroll = ({ children }) => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window,
+  });
+
+  return React.cloneElement(children, {
+    // elevation: trigger ? 4 : 0,
+    color: trigger ? 'secondary' : 'transparent',
+  });
+};
 
 const Header = () => (
-  <Box m={4}>
-    <Grid container alignItems="center" justify="space-between" spacing={4}>
-      <Grid item>
-        <Typography>Bentley Group</Typography>
-      </Grid>
-      <Grid item>
-        <Button color="inherit" href="#about" component={Link}>
-          About Us
-        </Button>
-        <Button color="inherit" href="#research" component={Link}>
-          Research
-        </Button>
-        <Button color="inherit" href="#software" component={Link}>
-          Software
-        </Button>
-        <Button color="inherit" href="#team" component={Link}>
-          Team
-        </Button>
-        <Button color="inherit" href="#publications" component={Link}>
-          Publications
-        </Button>
-      </Grid>
-    </Grid>
-  </Box>
+  <ElevationScroll>
+    <AppBar position="sticky" color="transparent" elevation={0}>
+      <Toolbar variant="dense">
+        <Grid container alignItems="center" justify="space-between" spacing={4}>
+          <Grid item>
+            <Typography>Bentley Group</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" href="#about" component={Link}>
+              About Us
+            </Button>
+            <Button color="inherit" href="#research" component={Link}>
+              Research
+            </Button>
+            <Button color="inherit" href="#software" component={Link}>
+              Software
+            </Button>
+            <Button color="inherit" href="#team" component={Link}>
+              Team
+            </Button>
+            <Button color="inherit" href="#publications" component={Link}>
+              Publications
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  </ElevationScroll>
 );
 
 export default Header;
