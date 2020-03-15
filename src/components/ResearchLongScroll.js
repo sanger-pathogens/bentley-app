fdca23c898c14ac23340445cc9efe38d8143fdb5
Gareth Peat <gp16@sanger.ac.uk>
2020-03-15 11:59:15 +0000

    Remove unused imports

--- src/components/ResearchLongScroll.js
+++ src/components/ResearchLongScroll.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
 import raw from 'raw.macro';
 
-import Page from './Page';
 import Content from './Content';
 
 const mdResearch = raw('../content/research-themes.md');
