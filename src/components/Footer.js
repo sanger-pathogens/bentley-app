24bb3e22ace302d13287f20dcb53debb3c3c09d2
Gareth Peat <gp16@sanger.ac.uk>
2020-03-15 11:34:52 +0000

    Add Funders section to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,30 @@ import AlternateEmailOutlinedIcon from '@material-ui/icons/AlternateEmailOutline
 import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
+import Funders from './Funders';
+
 const Footer = () => (
-  <Box m={4}>
-    <Grid container alignItems="center" justify="space-between" spacing={4}>
-      <Grid item>
-        <img src="SangerLogo.9423243b.png" width="166" alt="Sanger logo" />
+  <React.Fragment>
+    <Funders />
+    <Box m={4}>
+      <Grid container alignItems="center" justify="space-between" spacing={4}>
+        <Grid item>
+          <img src="SangerLogo.9423243b.png" width="166" alt="Sanger logo" />
+        </Grid>
+        <Grid item>
+          <IconButton color="inherit" target="_blank" href="#" component={Link}>
+            <AlternateEmailOutlinedIcon />
+          </IconButton>
+          <IconButton color="inherit" target="_blank" href="#" component={Link}>
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton color="inherit" target="_blank" href="#" component={Link}>
+            <TwitterIcon />
+          </IconButton>
+        </Grid>
       </Grid>
-      <Grid item>
-        <IconButton color="inherit" target="_blank" href="#" component={Link}>
-          <AlternateEmailOutlinedIcon />
-        </IconButton>
-        <IconButton color="inherit" target="_blank" href="#" component={Link}>
-          <LinkedInIcon />
-        </IconButton>
-        <IconButton color="inherit" target="_blank" href="#" component={Link}>
-          <TwitterIcon />
-        </IconButton>
-      </Grid>
-    </Grid>
-  </Box>
+    </Box>
+  </React.Fragment>
 );
 
 export default Footer;
