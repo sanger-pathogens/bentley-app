f8db1fad0bd49c9220559a7cb9bc50e6c0059192
Gareth Peat <gp16@sanger.ac.uk>
2020-03-26 10:46:11 +0000

    Remove unused code

--- src/components/PageTreeOfLife.js
+++ src/components/PageTreeOfLife.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { Box, Typography, Link } from '@material-ui/core';
 
 import Page from './Page';
-// import ReseachLongScroll from './ResearchLongScroll';
 import TreeOfLife from './TreeOfLife';
-// import TeamLeftRight from './TeamLeftRight';
-// import TeamLeft from './TeamLeft';
 
 const PageTreeOfLife = () => {
   return (
@@ -22,12 +19,6 @@ const PageTreeOfLife = () => {
           species of interest section.
         </Typography>
         <TreeOfLife />
-        {/* <Typography variant="h2">Variant 1: Long Scroll</Typography>
-        <ReseachLongScroll /> */}
-        {/* <Typography variant="h2">Variant 2: Left-Right</Typography>
-        <TeamLeftRight />
-        <Typography variant="h2">Variant 3: Left</Typography>
-        <TeamLeft /> */}
       </Box>
     </Page>
   );
