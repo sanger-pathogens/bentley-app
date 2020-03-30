84e9f9f8fef733c3a3a93bcdcbf2cc8272c8808a
Gareth Peat <gp16@sanger.ac.uk>
2020-03-30 11:23:37 +0100

    Remove Grid suffix from Software and Team components

--- src/content/sections.js
+++ src/content/sections.js
@@ -5,8 +5,8 @@ import { Typography } from '@material-ui/core';
 import AboutUs from '../components/AboutUs';
 import ResearchThemes from '../components/ResearchThemes';
 import Publications from '../components/Publications';
-import SoftwareGrid from '../components/SoftwareGrid';
-import TeamGrid from '../components/TeamGrid';
+import Software from '../components/Software';
+import Team from '../components/Team';
 import Content from '../components/Content';
 
 const mdVisitors = raw('../content/visitors.md');
@@ -29,7 +29,7 @@ export default [
     label: 'Software',
     title: 'Our software',
     url: 'software',
-    contentComponent: SoftwareGrid,
+    contentComponent: Software,
   },
   {
     label: 'Team',
@@ -40,7 +40,7 @@ export default [
         <Typography variant="h4" style={{ paddingTop: '1rem' }}>
           Current members
         </Typography>
-        <TeamGrid />
+        <Team />
         <Typography variant="h4" style={{ paddingTop: '1rem' }}>
           Visitors
         </Typography>
