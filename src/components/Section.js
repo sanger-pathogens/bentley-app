6d5eafaa4775d0de0e2b18429a0323184c4ba4e6
Gareth Peat <gp16@sanger.ac.uk>
2020-04-01 17:39:58 +0100

    Add Section padding

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   gridContainer: {
     margin: 0,
-    padding: '24px',
+    padding: '0 24px',
     width: '100%',
     flex: '1 0 auto',
   },
