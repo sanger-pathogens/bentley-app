8d02949835638d07153d7f955f36a470e5eb032c
Gareth Peat <gp16@sanger.ac.uk>
2020-04-06 10:54:12 +0100

    Remove Collaborators text

--- src/components/Collaborators.js
+++ src/components/Collaborators.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import raw from 'raw.macro';
 
-import Markdown from './Markdown';
 import WorldMap from './WorldMap';
 
-const mdCollaborators = raw('../content/collaborators.md');
-
 const Collaborators = () => {
-  return (
-    <>
-      <Markdown md={mdCollaborators} />
-      <WorldMap />
-    </>
-  );
+  return <WorldMap />;
 };
 
 export default Collaborators;
