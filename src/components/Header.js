79fc49d10009998893db184d02ddf754ac532427
Gareth Peat <gp16@sanger.ac.uk>
2020-04-06 12:57:55 +0100

    Add navigation menu for small screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,63 @@ import {
   Grid,
   Button,
   Link,
+  Menu,
+  MenuItem,
+  IconButton,
   useScrollTrigger,
+  Hidden,
 } from '@material-ui/core';
+import { Menu as MenuIcon } from '@material-ui/icons';
+
+const NavigationFlat = ({ navigation }) => (
+  <React.Fragment>
+    {navigation.map((item, i) => (
+      <Button key={i} color="inherit" href={item.url} component={Link}>
+        {item.label}
+      </Button>
+    ))}
+  </React.Fragment>
+);
+
+const NavigationMenu = ({ navigation }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        edge="end"
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        color="inherit"
+        aria-label="menu"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {navigation.map(item => (
+          <MenuItem key={item.url} component={Link} href={item.url}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+};
 
 const Header = ({ navigation }) => {
   const trigger = useScrollTrigger({
@@ -31,11 +86,12 @@ const Header = ({ navigation }) => {
             ) : null}
           </Grid>
           <Grid item>
-            {navigation.map((item, i) => (
-              <Button key={i} color="inherit" href={item.url} component={Link}>
-                {item.label}
-              </Button>
-            ))}
+            <Hidden smDown>
+              <NavigationFlat navigation={navigation} />
+            </Hidden>
+            <Hidden mdUp>
+              <NavigationMenu navigation={navigation} />
+            </Hidden>
           </Grid>
         </Grid>
       </Toolbar>
