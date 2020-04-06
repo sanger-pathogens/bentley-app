340b680455c3f4b2d1139418d8e4f48381f94b8a
Gareth Peat <gp16@sanger.ac.uk>
2020-04-06 10:04:26 +0100

    Change image and attribution to Magnus photo

--- src/components/SplashImage.js
+++ src/components/SplashImage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box, Typography } from '@material-ui/core';
+import { Grid, Box, Typography, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ArrowDownward } from '@material-ui/icons';
 
@@ -7,7 +7,8 @@ import AnimatedTextPath from './AnimatedTextPath';
 
 const useStyles = makeStyles({
   splash: {
-    backgroundImage: 'url(morganlitup.jpg)',
+    backgroundImage:
+      'url(2048px-Sanger_Institute_and_Hinxton_Hall,_Cambridge,_UK.jpg)',
     backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat',
     backgroundAttachment: 'fixed',
@@ -19,6 +20,9 @@ const useStyles = makeStyles({
     left: 10,
     bottom: 10,
     overflow: 'hidden',
+  },
+  imageCreditLink: {
+    color: 'white',
   },
 });
 
@@ -48,7 +52,33 @@ const SplashImage = () => {
         align="left"
         className={classes.imageCredit}
       >
-        Image Credit: Sanger Institute, Genome Research Limited
+        {/* Image Credit: Sanger Institute, Genome Research Limited */}
+        <Link
+          className={classes.imageCreditLink}
+          href="https://commons.wikimedia.org/wiki/User:Magnus_Manske"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Magnus Manske
+        </Link>
+        ,{' '}
+        <Link
+          className={classes.imageCreditLink}
+          href="https://commons.wikimedia.org/wiki/File:Sanger_Institute_and_Hinxton_Hall,_Cambridge,_UK.jpg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sanger Institute and Hinxton Hall, Cambridge, UK
+        </Link>
+        ,{' '}
+        <Link
+          className={classes.imageCreditLink}
+          href="https://creativecommons.org/licenses/by-sa/3.0/legalcode"
+          target="_blank"
+          rel="license noopener noreferrer"
+        >
+          CC BY-SA 3.0
+        </Link>
       </Typography>
     </Box>
   );
