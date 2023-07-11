e042c765b8497cfda618174cd1062364a73ad554
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-07-11 11:39:18 +0100

    Adding new images and optimise image size

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,12 +3,22 @@ import { Box } from '@material-ui/core';
 
 const Gallery = () => (
   <Box>
+    <img src="/gallery/enjoying_sanger_bbq.jpg" alt="Enjoying Sanger BBQ" width="50%" />
+    <img src="/gallery/enjoying_meal_together.jpg" alt="Enjoying meal together" width="50%" />
+    <img src="/gallery/group_photo_at_hinxton_hall.jpeg" alt="Team at Hinxton Hall" width="50%" />
+    <img src="/gallery/team_meeting_at_hinxton_hall.jpg" alt="Team Meeting at Hinxton Hall" width="50%" />
+    <img src="/gallery/chilling_at_hinxton_hall.jpeg" alt="Chilling at Hinxton Hall" width="50%" />
+    <img src="/gallery/team_found_a_sustainability_rock.jpeg" alt="Team found a suntainability rock on the Wellcome Genome Campus" width="50%" />
+    <img src="/gallery/drawing_top_trumps_cards_for_pneumo.jpg" alt="Drawing Top Trumps Cards for Pneumo" width="50%" />
+    <img src="/gallery/just_another_team_meeting.jpg" alt="Just another team meeting" width="50%" />
+    <img src="/gallery/enjoying_punting.jpeg" alt="Enjoying punting at River Cam" width="50%" />
+    <img src="/gallery/taking_a_walk_during_retreat.jpeg" alt="Taking a walk during retreat under usual British weather" width="50%" />
     <img src="/gallery/Get together at Jesus Green.jpg" alt="Get together at Jesus Green" width="50%" />
-    <img src="/gallery/IMG_4013.png" alt="Lunch on the Wellcome Genome Campus grounds" width="50%" />
+    <img src="/gallery/IMG_4013.jpg" alt="Lunch on the Wellcome Genome Campus grounds" width="50%" />
     <img src="/gallery/Narender_s first lunch at Sulston building with Steph since lockdown.jpg" alt="Narender's first lunch at Sulston building with Steph since lockdown" width="50%" />
     <img src="/gallery/Steph_Nida_Meeting during pandemics on Genome campus.jpg" alt="Steph and Nida meeting during pandemics on Genome campus" width="50%" />
-    <img src="/gallery/IMG_4012.png" alt="First group lab meeting after the pandemic" width="50%" />
-    <img src="/gallery/IMG_4014.png" alt="Steph and Uzma talking from a social distance" width="50%" />
+    <img src="/gallery/IMG_4012.jpg" alt="First group lab meeting after the pandemic" width="50%" />
+    <img src="/gallery/IMG_4014.jpg" alt="Steph and Uzma talking from a social distance" width="50%" />
     <img src="/gallery/Get together in front of King_s college.JPG" alt="Get together in front of King's college" width="100%" />
   </Box>
 );
